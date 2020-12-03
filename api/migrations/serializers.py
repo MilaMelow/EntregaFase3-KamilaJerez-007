dab269fb5db373ead2720954ef882f14560d70b4
Kamila <kamilajerezherrada@gmail.com>
2020-12-03 08:51:59 -0300

    Final

--- api/migrations/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-class UserSerializer(serializers.Serializer):
-    id = serializers.ReadOnlyField()
-    username = serializers.CharField()
-    email = serializers.EmailField()
-    password = serializers.CharField()
